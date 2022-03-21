9a9f5f2f7dc4d51006b3f45c1cf14b1a93f6b516
jackhotgit <101988286+jackhotgit@users.noreply.github.com>
2022-03-21 15:02:35 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.sxtc7fvm01':}
   dockeragent::node {'db.sxtc7fvm01':}
+  dockeragent::node {'minetest.sxtc7fvm01':}
   host { 'web.sxtc7fvm01':
     ensure => present,
     ip     => '172.18.0.2',
