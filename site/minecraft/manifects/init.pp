5b8009b8408cf75c8564085b7954eef4bd03da3e
jackhotgit <101988286+jackhotgit@users.noreply.github.com>
2022-03-21 16:11:39 -0400

    Create init.pp

---
+++ site/minecraft/manifects/init.pp
@@ -0,0 +1,13 @@
+class minecraft {
+  file {'/opt/minecraft':
+  ensure => directory,
+  }
+  file { '/opt/minecraft/server.jar'
+  ensure => file,
+  source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar'
+  }
+  file { '/opt/jdk-17_linux-x64_bin.rpm'
+  ensure => file,
+  source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm'
+  }
+}
