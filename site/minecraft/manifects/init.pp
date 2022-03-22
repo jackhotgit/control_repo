4cc65633301434db0cc2c9b1c4652e78769af31b
jackhotgit <101988286+jackhotgit@users.noreply.github.com>
2022-03-22 14:13:08 -0400

    Update init.pp

--- site/minecraft/manifects/init.pp
+++ site/minecraft/manifects/init.pp
@@ -4,22 +4,26 @@ class minecraft {
   }
   file { '/opt/minecraft/server.jar'
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar'
+    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
   }
 #  file { '/opt/jdk-17_linux-x64_bin.rpm'
 #  ensure => file,
-#  source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm'
+#  source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm',
 #  }
   package { 'java':
     ensure => present ,
-    source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm'
+    source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm',
   }
   file { '/opt/minecraft/eula.txt'
     ensure => file,
-    content => 'eula=true'
+    content => 'eula=true',
   }
   file { '/etc/systemd/system/minecraft.service'
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service'
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
   }
 }
